8394ac99c579f02dc7110c104080f9fe89d981e2
sanjay.saget-gobardhan <sanjay.saget-gobardhan@epita.fr>
2026-02-11 17:40:40 +0100

    UPDATE : not an upgrade but added a deadcode test version of the main_game function, the results of the tests are detailled in the README

--- One_Player_RPC_game/One_Player_RPC_game.py
+++ One_Player_RPC_game/One_Player_RPC_game.py
@@ -5,6 +5,7 @@ Created on Mon Feb  9 15:10:55 2026
 @author: sandh
 """
 from random import randint
+from matplotlib import pyplot as plt
 
 
 
@@ -45,13 +46,28 @@ def handshape_translation(handshape_code):
 
 def main_game():
     valid_commands = ["pierre", "feuille", "ciseaux", "exit", "score"]
+    valid_shapes = ["pierre", "feuille", "ciseaux"]
+    num_of_tests = 10000
+    tests_choices = [valid_shapes[randint(0, 2)] for l in range(num_of_tests)]
     handshapes = []
+    player_score_track = []
+    bot_score_track = []
+    played_games_track = []
     init_choice_list(handshapes)
     player_score = 0
     bot_score = 0
-    
-    while(True):
-        player_hand = str(input("enter the handshape : "))
+    played_games = 0
+    #while(True)
+    for k in range(num_of_tests):
+        if played_games % 2 == 0:
+            player_score_track.append(player_score)
+            bot_score_track.append(bot_score)
+            played_games_track.append(played_games)
+            if len(played_games_track) > 0:
+                plt.plot(player_score_track,"red", marker = 'o')
+                plt.plot(bot_score_track,"blue", marker = 'o')
+                plt.show()
+        player_hand = tests_choices[k]#str(input("enter the handshape : "))
         if player_hand not in valid_commands:
             print(f"Command Error : {player_hand} is not a valid command")
             continue
@@ -72,12 +88,15 @@ def main_game():
              (player_hand == "ciseaux" and real_choice == "pierre"):
             print("Bot won !")
             bot_score += 1
+            played_games += 1
         else:
             print("Player won !")
             player_score += 1
+            played_games += 1
             mapping = {"pierre": 0, "feuille": 1, "ciseaux": 2}
             adapt_bot_decision(handshapes, bot_choice_idx, mapping[player_hand])
-        
+    print(f"Player score : {player_score}")
+    print(f"Bot score : {bot_score}")
          
              
         
--- One_Player_RPC_game/README.md
+++ One_Player_RPC_game/README.md
@@ -6,3 +6,12 @@ It is actually rather simple, the bot chooses an handshape represented by and in
 Whenever the bot looses, he will take your choice in the shape of the associated integer and replace the instance he chose in his list by the value that would have beated your choice thus making it more likely to beat you if you make the same choice.
 It works rather well and the bot will start to win or draw more often quite fast, but you could easily mislead him by playing the same handshape multiples times in a row and knowing how its decision algorithm works basically allows you to predict his choices basically making it almost impossible for him to win,but if you play randomly it should work just fine.
 
+Update:
+After thourough (arguable) testing of the bot, it turns out that the number of played games does have an effect on
+the bot performances. Under 100 games with a human (me) it wins around 75% of the time. However when tested with a
+set of shapes thats was randomly generated (randint once again) its winning rate drops to approximately 50% but 
+its progression is more stable, what I mean by that is that if it takes the lead at some point it will tend
+to keep it until the end, but as it progresses in the number of games it becomes very senstive to biases and 
+misleading. I tested it on 100, 500, 1000, 5000 consecutives games, I tried with 10000 but my IDE (spyder) crashed. 
+But the scores are usually quite close regardless of the winner, that is if the bot didnt experienced a big bias 
+beforehand.
